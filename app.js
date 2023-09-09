0e957606556ed706092b416be32aaeaa0cbee79d
Pulkit Shringi <pulkitshringi@yahoo.com>
2023-09-09 21:57:52 +0530

    updated

--- app.js
+++ app.js
@@ -6,13 +6,22 @@ const API_KEY ="e60f5cb54447854537c19bc5c0fa1688";
 const getCityCoordinates = () =>{
 const cityName = cityInput.value.trim();
 if(!cityName) return;
-const GEOCODING_API_URL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`;
+const GEOCODING_API_URL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${API_KEY}`;
 fetch(GEOCODING_API_URL)
 .then((res)=> res.json()).then(data=>{
     if(data.length==0){
        return alert(`Your City Name : ${cityName} is not valid.`);
     }
-    console.log(data);
+    else{
+    const uniqueForecastDays = [];
+const fiveDaysForecastDays = data.list.filter(forecast => {
+const forecastDate = new Date (forecast.dt_txt).getDate();
+if(!uniqueForecastDays.includes(forecastDate)) {
+return uniqueForecastDays.push(forecastDate);
+}
+});
+console.log(fiveDaysForecastDays);
+    }
 }).catch(()=>{
     alert("An error occurred while fetching the coordinates!");
 })
