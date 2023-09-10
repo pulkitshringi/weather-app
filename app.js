ab19459cd78784b5c1b0159324e984300f0aab9f
Pulkit Shringi <pulkitshringi@yahoo.com>
2023-09-10 14:10:43 +0530

    final touchups

--- app.js
+++ app.js
@@ -5,8 +5,13 @@ const weatherCards = document.querySelector(".weather-cards");
 const currentWeatherCard = document.querySelector(".current-weather");
 const API_KEY ="e60f5cb54447854537c19bc5c0fa1688";
 const loadingIcon = document.querySelectorAll("i");
-let city;
+const celc = document.querySelector(".celcius");
+const fahren = document.querySelector(".fahren");
+let city="";
+let flag = 1;
+let flag2=1;
 const addCurrWeather = (weatherInfo)=>{
+    if(flag==1){
     return `<div class="details">
     <h2>${city}</h2>
     <h4>${(weatherInfo.main.temp - 273.15).toFixed(2)}°C</h4>
@@ -17,8 +22,22 @@ const addCurrWeather = (weatherInfo)=>{
     <img src="https://openweathermap.org/img/wn/${weatherInfo.weather[0].icon}@2x.png" alt="weather-icon">
     <h4>${weatherInfo.weather[0].description}</h4>
 </div>`
+    }
+    else if(flag==0){
+        return `<div class="details">
+    <h2>${city}</h2>
+    <h4>${((weatherInfo.main.temp - 273.15)*1.8+32).toFixed(2)}°F</h4>
+    <h4>${weatherInfo.wind.speed} M/S</h4>
+    <h4>Humidity: ${weatherInfo.main.humidity}%</h4>
+</div>
+<div class="icon">
+    <img src="https://openweathermap.org/img/wn/${weatherInfo.weather[0].icon}@2x.png" alt="weather-icon">
+    <h4>${weatherInfo.weather[0].description}</h4>
+</div>`
+    }
 }
 const addWeatherDetails = (weatherInfo)=>{
+    if(flag==1){
 return `<li class="card">
 <h3>(${weatherInfo.dt_txt.split(" ")[0].split("-")[2]+"/"+ weatherInfo.dt_txt.split(" ")[0].split("-")[1]+"/"+weatherInfo.dt_txt.split(" ")[0].split("-")[0]})</h3>
 <img src="https://openweathermap.org/img/wn/${weatherInfo.weather[0].icon}@2x.png" alt="weather-icon">
@@ -27,33 +46,61 @@ return `<li class="card">
 <h4>Humidity: ${weatherInfo.main.humidity}%</h4>
 </li>` 
 }
+else if(flag==0){
+    return `<li class="card">
+<h3>(${weatherInfo.dt_txt.split(" ")[0].split("-")[2]+"/"+ weatherInfo.dt_txt.split(" ")[0].split("-")[1]+"/"+weatherInfo.dt_txt.split(" ")[0].split("-")[0]})</h3>
+<img src="https://openweathermap.org/img/wn/${weatherInfo.weather[0].icon}@2x.png" alt="weather-icon">
+<h4>Temperature : ${((weatherInfo.main.temp - 273.15)*1.8+32).toFixed(2)}°F</h4>
+<h4>Wind: ${weatherInfo.wind.speed} M/S</h4>
+<h4>Humidity: ${weatherInfo.main.humidity}%</h4>
+</li>` 
+}
+}
 const getCityCoordinates = () =>{
+    if(cityInput.value!=""){
+    city=cityInput.value.trim();
+    }   
+    if(cityInput.value=="" && city=="")
+        return;
     loadingIcon[0].setAttribute("class","fa fa-spinner fa-spin");
 const cityName = cityInput.value.trim();
-if(!cityName) return;
-const GEOCODING_API_URL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${API_KEY}`;
+if(flag2==0){
+if(!cityName) {alert("Error! CityName cannot be empty.");
+loadingIcon[0].setAttribute("class","");
+    return;
+}
+}
+const GEOCODING_API_URL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}`;
 fetch(GEOCODING_API_URL)
 .then((res)=> res.json()).then(data=>{
     if(data.length==0){
-       return alert(`Your City Name : ${cityName} is not valid.`);
+       return alert(`Your City Name : ${city} is not valid.`);
     }
     else{
+        console.log(data);
     const uniqueForecastDays = [];
-const fiveDaysForecastDays = data.list.filter(forecast => {
+    const uniqueForecastHours = [];
+const fiveDaysForecast = data.list.filter(forecast => {
 const forecastDate = new Date (forecast.dt_txt).getDate();
-if(!uniqueForecastDays.includes(forecastDate)) {
+const forecastTime = new Date (forecast.dt_txt).getHours();
+if(uniqueForecastHours[0]===undefined)
+    uniqueForecastHours[0] = forecastTime;
+if(!uniqueForecastDays.includes(forecastDate)&& forecastTime==uniqueForecastHours[0]){
+    console.log(forecastTime);
+    uniqueForecastHours.push(forecastTime);
 return uniqueForecastDays.push(forecastDate);
 }
 });
-city=cityInput.value.trim();
+console.log(fiveDaysForecast);
 cityInput.value = "";
 cityInput.setAttribute("placeholder","Enter another city");
 weatherCards.innerHTML="";
 currentWeatherCard.innerHTML="";
-fiveDaysForecastDays.forEach((weatherInfo,index) => {
+fiveDaysForecast.forEach((weatherInfo,index) => {
+    console.log(weatherInfo.weather[0].id);
+console.log(weatherInfo.weather[0].icon);
     if(index==0)
     currentWeatherCard.insertAdjacentHTML("afterbegin",addCurrWeather(weatherInfo));
-    else
     weatherCards.insertAdjacentHTML("beforeend",addWeatherDetails(weatherInfo));
 });
 loadingIcon[0].setAttribute("class","");
@@ -90,8 +137,28 @@ const getUserCoordinates = () => {
         }
     )
 };
-
+const celcselect = ()=>{
+    celc.style.color="goldenrod";
+    celc.style.fontSize = "1.2rem";
+    fahren.style.color="black";
+    fahren.style.fontSize = "1rem";
+    flag = 1;
+    flag2=1;
+    getCityCoordinates();
+};
+const fahrenselect = ()=>{
+    fahren.style.color="goldenrod";
+    fahren.style.fontSize = "1.2rem";
+    celc.style.color="black";
+    celc.style.fontSize = "1rem";
+    flag = 0;
+    flag2=1;
+    getCityCoordinates();
+};
+celc.addEventListener("click",celcselect);
+fahren.addEventListener("click",fahrenselect);
 locationButton.addEventListener("click",getUserCoordinates);
+searchButton.addEventListener("click",()=>{flag2=0});
 searchButton.addEventListener("click",getCityCoordinates);
 cityInput.addEventListener("keyup",(keyname)=>{
     if(keyname.key=="Enter")
