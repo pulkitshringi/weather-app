4590f2cf4b1dd6c3390d573e092d633808ac7d5f
Pulkit Shringi <pulkitshringi@yahoo.com>
2023-09-10 05:45:29 +0530

    almost complete

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
 const searchButton = document.querySelector(".search-btn");
+const locationButton = document.querySelector(".location-btn");
 const cityInput = document.querySelector(".city-input");
-
+const weatherCards = document.querySelector(".weather-cards");
+const currentWeatherCard = document.querySelector(".current-weather");
 const API_KEY ="e60f5cb54447854537c19bc5c0fa1688";
-
+let city;
+const addCurrWeather = (weatherInfo)=>{
+    return `<div class="details">
+    <h2>${city}</h2>
+    <h4>${(weatherInfo.main.temp - 273.15).toFixed(2)}°C</h4>
+    <h4>${weatherInfo.wind.speed} M/S</h4>
+    <h4>Humidity: ${weatherInfo.main.humidity}%</h4>
+</div>
+<div class="icon">
+    <img src="https://openweathermap.org/img/wn/${weatherInfo.weather[0].icon}@2x.png" alt="weather-icon">
+    <h4>${weatherInfo.weather[0].description}</h4>
+</div>`
+}
+const addWeatherDetails = (weatherInfo)=>{
+return `<li class="card">
+<h3>(${weatherInfo.dt_txt.split(" ")[0].split("-")[2]+"/"+ weatherInfo.dt_txt.split(" ")[0].split("-")[1]+"/"+weatherInfo.dt_txt.split(" ")[0].split("-")[0]})</h3>
+<img src="https://openweathermap.org/img/wn/${weatherInfo.weather[0].icon}@2x.png" alt="weather-icon">
+<h4>Temperature : ${(weatherInfo.main.temp - 273.15).toFixed(2)}°C</h4>
+<h4>Wind: ${weatherInfo.wind.speed} M/S</h4>
+<h4>Humidity: ${weatherInfo.main.humidity}%</h4>
+</li>` 
+}
 const getCityCoordinates = () =>{
 const cityName = cityInput.value.trim();
 if(!cityName) return;
@@ -20,11 +43,52 @@ if(!uniqueForecastDays.includes(forecastDate)) {
 return uniqueForecastDays.push(forecastDate);
 }
 });
-console.log(fiveDaysForecastDays);
+city=cityInput.value.trim();
+cityInput.value = "";
+cityInput.setAttribute("placeholder","Enter another city");
+weatherCards.innerHTML="";
+currentWeatherCard.innerHTML="";
+fiveDaysForecastDays.forEach((weatherInfo,index) => {
+    if(index==0)
+    currentWeatherCard.insertAdjacentHTML("afterbegin",addCurrWeather(weatherInfo));
+    else
+    weatherCards.insertAdjacentHTML("beforeend",addWeatherDetails(weatherInfo));
+});
     }
-}).catch(()=>{
-    alert("An error occurred while fetching the coordinates!");
 })
+.catch(()=>{
+    alert("An error occurred while fetching the coordinates!");
+});
 }
+const getUserCoordinates = () => { 
+    cityInput.value="";
+    navigator.geolocation.getCurrentPosition(
+        (position) => {
+            const { latitude , longitude } = position.coords;
+             const Reverse_Geocoding_URL = `http://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=1&appid=${API_KEY}`;
+             fetch(Reverse_Geocoding_URL)
+             .then((res)=>{
+                return res.json();
+             })
+             .then((data)=>{
+               cityInput.value=data[0].name;
+                getCityCoordinates();
+             })
+             .catch(()=>{
+                alert("An error occured while fetching the city!");
+             })
+        },
+        (error) => {
+           if(error.code == 1){
+            alert("Geolocation request denied. Please reset location permission to grant access again.");
+           }
+        }
+    )
+};
 
+locationButton.addEventListener("click",getUserCoordinates);
 searchButton.addEventListener("click",getCityCoordinates);
+cityInput.addEventListener("keyup",(keyname)=>{
+    if(keyname.key=="Enter")
+        getCityCoordinates();
+})
